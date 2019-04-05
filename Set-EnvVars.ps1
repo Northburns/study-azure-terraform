f70ab70b12a54c1f99167bdcca85f228b90eecf4
Norhtburns <northburns@gmail.com>
2019-04-05 15:33:56 +0300

    document envvars

--- Set-EnvVars.ps1
+++ Set-EnvVars.ps1
@@ -1,8 +1,14 @@
 # Set these to that csv file:
+#
+# (Used by Terraform)
 # ARM_CLIENT_ID
 # ARM_SUBSCRIPTION_ID
 # ARM_TENANT_ID
 # ARM_CLIENT_SECRET
+#
+# (used by K8s)
+# TF_VAR_client_id
+# TF_VAR_client_secret
 
 Import-Csv "Set-EnvVars.csv" | Foreach-Object { 
 
